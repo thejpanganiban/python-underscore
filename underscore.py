671be7f5dd99684608924b419a7596a7ef1d60bc
Jesse Panganiban <me@jpanganiban.com>
2012-07-03 13:54:05 +0800

    Major refactor to enable extending the UnderscoreObject class

--- underscore.py
+++ underscore.py
@@ -1,38 +1,34 @@
 class UnderscoreObject(object):
 
-  current_value = None
-  args = None
-  kwargs = None
   is_chain = False
 
-  def __init__(self, value=None, is_chain=False, *args, **kwargs):
-    if value:
-      self.current_value = value
+  def __init__(self, value=None, *args, **kwargs):
+    self.current_value = value
 
-    if is_chain:
-      self.is_chain = is_chain
-
-    self.args = args
-    self.kwargs = kwargs
-
-  def value(self):
-    return self.current_value
-
-  def each(self, func):
+  def each(self, value=None, func=None):
+    self.current_value = self.current_value or value
+    func = value if callable(value) else func
     result = [func(item) for item in self.current_value]
     if self.is_chain:
       self.current_value = result
       return self
     return result
 
-  def filter(self, func):
+  def filter(self, value=None, func=None):
+    self.current_value = self.current_value or value
+    func = value if callable(value) else func
     result = [item for item in self.current_value if func(item)]
     if self.is_chain:
       self.current_value = result
       return self
     return result
 
-  def chain(self):
+  def value(self):
+    return self.current_value
+
+  def chain(self, value=None):
+    if value:
+      self.current_value = value
     self.is_chain = True
     return self
 
@@ -41,34 +37,30 @@ class Underscore(object):
 
   klass = None
   mixins = {}
+  methods = [
+        'each',
+        'filter',
+        'chain',
+      ]
 
   def __init__(self, underscore_class=UnderscoreObject):
     self.klass = underscore_class
+    self._set_methods()
 
-  # OOP-Style calls
+  def _set_methods(self):
+    for method_name in self.methods:
+      setattr(self, method_name, self._proxy_method(method_name))
+
+  def _proxy_method(self, method_name):
+    def wrapper(*fargs, **fkwargs):
+      return getattr(self.klass(*fargs, **fkwargs), method_name)
+    return wrapper()
+
   def __call__(self, *args):
     return self.klass(*args)
-
-  def _proxy_mixin(self, mixin_name):
-    def mixin_func(*args, **kwargs):
-      # We're also passing the args and kwargs in the klass object instance for
-      # later use. (or so that it could be accessed by the function inside)
-      return getattr(self.klass(*args, **kwargs), mixin_name)(*args, **kwargs)
-    return mixin_func
 
   def mixin(self, mixin_dict):
     self.mixins.update(mixin_dict)
     for mixin_name, mixin_func in self.mixins.iteritems():
-      # Used for OOP-Style calls.
       setattr(self.klass, mixin_name, staticmethod(mixin_func))
-      # Used for functional-style calls.
-      setattr(self, mixin_name, self._proxy_mixin(mixin_name))
-
-  def each(self, value, func):
-    return self.klass(value).each(func)
-
-  def filter(self, value, func):
-    return self.klass(value).filter(func)
-
-  def chain(self, value):
-    return self.klass(value, is_chain=True)
+      setattr(self, mixin_name, self._proxy_method(mixin_name))
