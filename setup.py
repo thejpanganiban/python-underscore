2601cea0c30bb06d84ad6830946dc383e47eda47
Jesse Panganiban <me@jpanganiban.com>
2012-07-03 19:32:06 +0800

    Made underscore and tests packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name="Underscore",
       description="Underscore port in Python",
       version="0.1.0",
-      py_modules=['underscore'],
+      packages=find_packages(),
       install_requires=[],
       test_suite='tests')
